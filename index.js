802448f13c690332e50bf375270dd21ca30e6ec9
Addu Kay <adarshackerm4n@gmail.com>
2024-05-20 19:09:21 +0530

    app complete

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-const secContainer = document.getElementsByClassName("secContainer")[0];
-const minContainer = document.getElementsByClassName("minContainer")[0];
-const startButton = document.getElementById("start");
-const replayButton = document.getElementById("replay");
-const settingsButton = document.getElementById("settings");
+const secContainer = document.getElementsByClassName("seconds")[0];
+const minContainer = document.getElementsByClassName("minutes")[0];
+const startButton = document.getElementById("start-button");
+const replayButton = document.getElementById("replay-button");
+const settingsButton = document.getElementById("settings-button");
+const closeButton = document.getElementById("close-button");
+const saveButton = document.getElementById("save-button");
 
-// store the time id to stop the setInterval function
+// Store the time id to stop the setInterval function
 let timeInterval;
 
-//milliseconds elapsed since the button was clicked
+// Milliseconds elapsed since the button was clicked
 let elapsedMilliseconds;
 
-// pomodoro time to countdown
+// Pomodoro time to countdown
 let pomodoroTime = 24;
 
 startButton.addEventListener("click", startTimer);
 replayButton.addEventListener("click", reset);
-settingsButton.addEventListener("click", addClass);
+settingsButton.addEventListener("click", showSettingsModal);
+closeButton.addEventListener("click", closeSettingsModal);
+saveButton.addEventListener("click", savePomodoroTime);
 
 function startTimer() {
   let clickTime = Date.now();
   timeInterval = setInterval(() => {
     const currentTime = Date.now();
 
-    // milliseconds elapsed after the button was clicked
+    // Milliseconds elapsed after the button was clicked
     elapsedMilliseconds = currentTime - clickTime;
 
-    // seconds elapsed after the button was clicked and keep the range between 0-59
+    // Seconds elapsed after the button was clicked and keep the range between 0-59
     let elapsedSeconds = Math.floor(elapsedMilliseconds / 1000) % 60;
 
-    // minutes elapsed after the button was clicked
+    // Minutes elapsed after the button was clicked
     let elapsedMinutes = Math.floor(elapsedMilliseconds / (60 * 1000));
 
-    // start the seconds from 59 and end with 0
+    // Start the seconds from 59 and end with 0
     elapsedSeconds = 59 - elapsedSeconds;
 
-    // start with pomodoro Time and end with 0
+    // Start with pomodoroTime and end with 0
     elapsedMinutes = pomodoroTime - elapsedMinutes;
 
-    // if the elapsedSecond is  single digit, add padding of 0 otherwise elapsedSecond
+    // If the elapsedSeconds is a single digit, add padding of 0 otherwise use elapsedSeconds
     secContainer.innerHTML =
       elapsedSeconds.toString().length == 2
         ? elapsedSeconds
         : `0${elapsedSeconds}`;
 
-    // if the elapsedMinute is  single digit, add padding of 0 otherwise elapsedMinute
+    // If the elapsedMinutes is a single digit, add padding of 0 otherwise use elapsedMinutes
     minContainer.innerHTML =
       elapsedMinutes.toString().length == 2
         ? elapsedMinutes
@@ -56,12 +60,28 @@ function startTimer() {
   }, 100);
 }
 
+// reset time back to pomodoroTime
 function reset() {
   clearInterval(timeInterval);
-  document.querySelector(".minContainer").innerHTML = pomodoroTime + 1;
-  document.querySelector(".secContainer").innerHTML = "00";
+  document.querySelector(".minutes").innerHTML = pomodoroTime + 1;
+  document.querySelector(".seconds").innerHTML = "00";
 }
 
-function addClass(event) {
-  document.getElementsByClassName("offmodal")[0].classList.remove("offmodal");
+// show settingsModal
+function showSettingsModal() {
+  const settingsModal = document.querySelector(".settings-modal");
+  settingsModal.classList.add("show");
+}
+
+// close settingsModal
+function closeSettingsModal() {
+  const settingsModal = document.querySelector(".settings-modal");
+  settingsModal.classList.remove("show");
+}
+
+function savePomodoroTime() {
+  const settingsModal = document.querySelector(".settings-modal");
+  settingsModal.classList.remove("show");
+  pomodoroTime = Number(document.querySelector("input").value) - 1;
+  reset();
 }
