782229de4220fa4fb8f2656317c0020a02d80d68
Addu Kay <adarshackerm4n@gmail.com>
2024-05-25 19:07:07 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const secContainer = document.getElementsByClassName("seconds")[0];
 const minContainer = document.getElementsByClassName("minutes")[0];
 const startButton = document.getElementById("start-button");
-const replayButton = document.getElementById("replay-button");
-const settingsButton = document.getElementById("settings-button");
-const closeButton = document.getElementById("close-button");
-const saveButton = document.getElementById("save-button");
 const alarm = new Audio("./Audio/alarm.mp3");
 
 // Store the time id to stop the setInterval function
@@ -17,22 +13,15 @@ let elapsedMilliseconds;
 let pomodoroTime = 24;
 
 // to check whether the timer is running
-let isQuitting = true;
-
-startButton.addEventListener("click", startTimer);
-replayButton.addEventListener("click", reset);
-settingsButton.addEventListener("click", showSettingsModal);
-closeButton.addEventListener("click", closeSettingsModal);
-saveButton.addEventListener("click", savePomodoroTime);
+let isRunning = false;
 
 function startTimer() {
   let clickTime = Date.now();
-  startButton.innerHTML = "Quit";
-  isQuitting = !isQuitting;
-  if (isQuitting) {
+  startButton.textContent = "Quit";
+  isRunning = !isRunning;
+  if (!isRunning) {
     clearInterval(timeInterval);
     reset();
-    startButton.innerHTML = "Start";
     return;
   }
 
@@ -55,38 +44,33 @@ function startTimer() {
     elapsedMinutes = pomodoroTime - elapsedMinutes;
 
     // If the elapsedSeconds is a single digit, add padding of 0 otherwise use elapsedSeconds
-    secContainer.innerHTML =
-      elapsedSeconds.toString().length == 2
-        ? elapsedSeconds
-        : `0${elapsedSeconds}`;
+    secContainer.textContent = String(elapsedSeconds).padStart(2, "0");
 
     // If the elapsedMinutes is a single digit, add padding of 0 otherwise use elapsedMinutes
-    minContainer.innerHTML =
-      elapsedMinutes.toString().length == 2
-        ? elapsedMinutes
-        : `0${elapsedMinutes}`;
+    minContainer.textContent = String(elapsedMinutes).padStart(2, "0");
 
-    if (minContainer.innerHTML == 0 && secContainer.innerHTML == 0) {
+
+    if (elapsedMinutes < 0) {
       clearInterval(timeInterval);
-      // new Audio("../Audio/alarm.mp3").play();
       alarm.play();
-      startButton.innerHTML = "Start";
-      minContainer.innerHTML =
-        `${pomodoroTime + 1}`.length == 2
-          ? pomodoroTime + 1
-          : `0${pomodoroTime + 1}`;
+      startButton.textContent = "Start";
+      let initialPomodoro = pomodoroTime + 1;
+      initialPomodoro = String(initialPomodoro).padStart(2, "0");
+      minContainer.textContent = initialPomodoro;
+      secContainer.textContent = "00";
     }
-  }, 100);
+  }, 10);
 }
 
 // reset time back to pomodoroTime
 function reset() {
   clearInterval(timeInterval);
-  minContainer.innerHTML =
-    `${pomodoroTime + 1}`.length == 2
-      ? pomodoroTime + 1
-      : `0${pomodoroTime + 1}`;
-  secContainer.innerHTML = "00";
+  startButton.textContent = "Start";
+  let initialPomodoro = pomodoroTime + 1;
+  initialPomodoro = String(initialPomodoro).padStart(2, "0");
+  minContainer.textContent = initialPomodoro;
+  secContainer.textContent = "00";
+  isRunning = false;
 }
 
 // show settingsModal
@@ -104,6 +88,11 @@ function closeSettingsModal() {
 function savePomodoroTime() {
   const settingsModal = document.querySelector(".settings-modal");
   settingsModal.classList.remove("show");
-  pomodoroTime = Number(document.querySelector("input").value) - 1;
+  const input = document.querySelector("input");
+  if (input.value == "" || input.value <= "0") {
+    input.value = 25;
+  }
+  console.log(input.value);
+  pomodoroTime = Number(input.value) - 1;
   reset();
 }
